931467e376792b677644f010b79c2996710a09e0
unknown <blixtdackhammar@gmail.com>
2020-05-20 11:58:32 +0200

    add another constructor and toString method

--- src/domain/User.java
+++ src/domain/User.java
@@ -3,11 +3,20 @@ package domain;
 public class User {
 	
 	private int id;
-	private String firstName;
+	private String name;
 	private String surname;
 	
 	public User() {
 		
+	}
+	
+	public User(String name, String surname) {
+		this.name = surname;
+		this.surname = surname;
+	}
+	
+	public String toString() {
+		return "User: " + this.name + " " + this.surname;
 	}
 
 }

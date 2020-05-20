35697fd0d056ca4e21d5b999529972dc68656fe8
AndersStationärDator <anders.maurits@hotmail.com>
2020-05-20 12:10:09 +0200

    added methods to implementation and service

--- src/usermanagement/UserManagementImplementation.java
+++ src/usermanagement/UserManagementImplementation.java
@@ -1,5 +1,30 @@
 package usermanagement;
 
-public class UserManagementImplementation {
+import java.util.List;
 
+import dataaccess.UserDataAccess;
+import domain.User;
+
+
+
+public class UserManagementImplementation implements UserManagementService {
+	
+	private UserDataAccess dao;
+	
+	@Override
+	public void registerUser(User user) {
+		
+
+	}
+
+	@Override
+	public List<User> getAllUsers() {
+		return dao.findAll();
+	}
+
+	@Override
+	public List<User> searchByUsername(String surname) {
+		return dao.findBySurname(surname);
+	}
+	
 }
--- src/usermanagement/UserManagementService.java
+++ src/usermanagement/UserManagementService.java
@@ -1,5 +1,12 @@
 package usermanagement;
 
-public class UserManagementService {
+import java.util.List;
 
+import domain.User;
+
+public interface UserManagementService {
+
+	public void registerUser(User user);
+	public List<User>getAllEmployees();
+	public List<User>searchBySurname(String surname);
 }

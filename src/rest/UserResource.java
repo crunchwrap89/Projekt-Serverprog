6ff080ecd27e1fcedb5cade105b3dea140a66d5b
unknown <blixtdackhammar@gmail.com>
2020-05-21 12:25:20 +0200

    corrected a mistake

--- src/rest/UserResource.java
+++ src/rest/UserResource.java
@@ -6,6 +6,7 @@ import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import com.sun.xml.internal.ws.wsdl.writer.document.Service;
@@ -24,5 +25,12 @@ public class UserResource {
 	@Produces("application/XML")
 	public List<User> getAllUsers() {
 		return service.getAllUsers();
+	}
+	
+	@GET
+	@Produces("application/XML")
+	@Path("{userNo}")
+	public User findUserById(@PathParam("UserNo") int id) {
+		return service.getById(id);
 	}
 }

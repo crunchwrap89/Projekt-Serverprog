f8294ef9819df684bd631699914102cbff30970a
unknown <blixtdackhammar@gmail.com>
2020-05-21 11:33:35 +0200

    add some annotations and code in UserResource

--- src/rest/UserResource.java
+++ src/rest/UserResource.java
@@ -2,11 +2,25 @@ package rest;
 
 import java.util.List;
 
-import domain.User;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 
+import com.sun.xml.internal.ws.wsdl.writer.document.Service;
+
+import domain.User;
+import usermanagement.UserManagementServiceLocal;
+
+@Stateless
+@Path("/users")
 public class UserResource {
 	
+	@Inject
+	private UserManagementServiceLocal service;
+	
+	@GET
 	public List<User> getAllUsers() {
-		return null;
+		return service.getAllUsers();
 	}
 }

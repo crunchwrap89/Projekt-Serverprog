6099fd0d8972f7df0f0f77654ed91a7cc72e92a7
unknown <blixtdackhammar@gmail.com>
2020-05-21 11:58:18 +0200

    add annotations

--- src/domain/User.java
+++ src/domain/User.java
@@ -1,14 +1,16 @@
 package domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
 @XmlRootElement
-public class User {
+public class User implements java.io.Serializable {
 	@Id
-	
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String name;
 	private String surname;

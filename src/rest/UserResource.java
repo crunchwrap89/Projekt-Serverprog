8aa5bc9a577f6d7ebcc90ad5aefe906d784a0b32
unknown <blixtdackhammar@gmail.com>
2020-05-25 14:07:40 +0200

    changed annotation on editUser method to @Put

--- src/rest/UserResource.java
+++ src/rest/UserResource.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -48,7 +49,7 @@ public class UserResource {
 		return user;
 	}
 	
-	@POST
+	@PUT
 	@Produces("application/XML")
 	@Consumes("application/XML")
 	@Path("{userNo}")
